1a4764c78a57f5318f1bb9a63b0df8e8c18be799
Ace Alrich <hex0@live.com>
2020-07-01 20:06:00 +0100

    green square

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,39 @@ use piston::event_loop::*;
 use piston::input::*;
 use piston::window::WindowSettings;
 
+struct Game {
+    gl: GlGraphics,
+}
+
+impl Game {
+    fn render(&mut self, arg: &RenderArgs) {
+        use graphics;
+
+        let GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
+        self.gl.draw(arg.viewport(), |_c, gl| {
+            graphics::clear(GREEN, gl);
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+
+    let mut window: GlutinWindow = WindowSettings::new("Snake Game", [200, 200])
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut game = Game {
+        gl: GlGraphics::new(opengl),
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            game.render(&r);
+        }
+    }
 }
