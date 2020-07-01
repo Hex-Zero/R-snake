6725a58c5fea55054a9446b3959a2d1e4e249aca
Ace Alrich <hex0@live.com>
2020-07-01 19:43:21 +0100

    imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::*;
+use piston::input::*;
+use piston::window::WindowSettings;
+
 fn main() {
     println!("Hello, world!");
 }
